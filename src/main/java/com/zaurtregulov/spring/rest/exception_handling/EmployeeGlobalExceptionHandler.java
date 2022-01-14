138730c6607e7372ad545a6fa27232381c472725
X-AnTa <mrspbusdriver@gmail.com>
2022-01-14 13:08:49 +0600

    SpringREST

---
+++ src/main/java/com/zaurtregulov/spring/rest/exception_handling/EmployeeGlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.zaurtregulov.spring.rest.exception_handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EmployeeGlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception) { // ResponseEntity - обёртка HTTP response, <EmployeeIncorrectData> - Тип объекта, который добавляется в HTTP response body, NoSuchEmployeeException - Exception, на который должен реагировать данный метод
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception) { // ResponseEntity - обёртка HTTP response, <EmployeeIncorrectData> - Тип объекта, который добавляется в HTTP response body, NoSuchEmployeeException - Exception, на который должен реагировать данный метод
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
+}
